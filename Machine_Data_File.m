7a90193c529998313a1dff930acc7999527b0a8e
Ben Pearre <bwpearre@gmail.com>
2017-01-18 16:15:40 -0500

    Update to use new PXIe card as beam controller.

--- Machine_Data_File.m
+++ Machine_Data_File.m
@@ -31,7 +31,7 @@ shutterOpenTime = 0.1;              % Time, in seconds, to delay following certa
 shutterNames = {'Shutter 1'};
 
 %% Beams
-beamDaqDevices = {'PXI1Slot4'};                            % Cell array of strings listing beam DAQs in the system. Each scanner set can be assigned one beam DAQ ex: {'PXI1Slot4'}
+beamDaqDevices = {'PXI1Slot6'};                            % Cell array of strings listing beam DAQs in the system. Each scanner set can be assigned one beam DAQ ex: {'PXI1Slot4'}
 
 % Define the parameters below for each beam DAQ specified above, in the format beamDaqs(N).param = ...
 beamDaqs(1).modifiedLineClockIn = '';           % one of {PFI0..15, ''} to which external beam trigger is connected. Leave empty for automatic routing via PXI/RTSI bus
@@ -39,12 +39,11 @@ beamDaqs(1).frameClockIn = '';                  % one of {PFI0..15, ''} to which
 beamDaqs(1).referenceClockIn = '';              % one of {PFI0..15, ''} to which external reference clock is connected. Leave empty for automatic routing via PXI/RTSI bus
 
 beamDaqs(1).chanIDs = [0 1];                       % Array of integers specifying AO channel IDs, one for each beam modulation channel. Length of array determines number of 'beams'.
-beamDaqs(1).displayNames = {'Channel 1 (780)' 'Channel 2 (1150)'};  % Optional string cell array of identifiers for each beam
+beamDaqs(1).displayNames = {'Ch1 780' 'Ch2 1140'};  % Optional string cell array of identifiers for each beam
 beamDaqs(1).voltageRanges = [2.2 2];                % Scalar or array of values specifying voltage range to use for each beam. Scalar applies to each beam.
 
 beamDaqs(1).calInputChanIDs = [0 1];               % Array of integers specifying AI channel IDs, one for each beam modulation channel. Values of nan specify no calibration for particular beam.
-% beamDaqs(1).calOffsets = 0.34;                    % Array of beam calibration offset voltages for each beam calibration channel
-beamDaqs(1).calOffsets = [0 0]; 
+beamDaqs(1).calOffsets = [0 0];                    % Array of beam calibration offset voltages for each beam calibration channel
 beamDaqs(1).calUseRejectedLight = [false false];        % Scalar or array indicating if rejected light (rather than transmitted light) for each beam's modulation device should be used to calibrate the transmission curve 
 beamDaqs(1).calOpenShutterIDs = [];             % Array of shutter IDs that must be opened for calibration (ie shutters before light modulation device).
 beamDaqs(1).referenceClockRate = 1e+07;
