a8d5eee695ed6bb034f24490e34ddb8d27d275e8
Ben Pearre <bwpearre@gmail.com>
2017-09-26 14:50:47 -0400

    Reset hexapod to zero rotation: - Control velocity - Eliminate MOM height dodge, since the new hexapod doesn't seem to need it - Fix bug: rotate about virtual centre, if one is defined

--- hexapod_reset_to_zero_rotation.m
+++ hexapod_reset_to_zero_rotation.m
@@ -1,27 +1,40 @@
 function hexapod_reset_to_zero_rotation(handles)
     global STL;
-    
-    pos = hexapod_get_position();
-    if any(abs(pos(4:6)) > 0.1)
-        foo = questdlg('Ok to un-rotate hexapod?', ...
-            'Stage setup', 'Yes', 'No', 'Yes');
-        switch foo
-            case 'Yes'
-                ;
-            case 'No'
-                return;
+
+    %% Some versions of PI Hexapod control crash z into the lens when doing
+    %% a big z rotation! So make sure it's okay to pull the lens up before
+    %% rotating.
+    %pos = hexapod_get_position();
+    %if any(abs(pos(4:6)) > 0.1)
+    %    foo = questdlg('Ok to un-rotate hexapod?', ...
+    %        'Stage setup', 'Yes', 'No', 'Yes');
+    %    switch foo
+    %        case 'Yes'
+    %            ;
+    %        case 'No'
+    %            return;
+    %   end
+    %end
+
+    %% For some reason, this can lead to about a 400-micron up-and-down. So
+    %% give us space!
+    %mompos = move('mom');
+    %minmommove = max(min(mompos(3), 400) - 1, 0)
+    %disp('Moving MOM to:');
+    %mompos - [0 0 minmommove]
+
+    %move('mom', mompos - [ 0 0 minmommove]);
+    hexapod_wait();
+
+    try
+        % If a rotation coordinate system is defined, then use it.
+        [~, b] = STL.motors.hex.C887.qKEN('');
+        if ~strcmpi(b(1:8), 'rotation')
+            STL.motors.hex.C887.KEN('rotation');
         end
     end
 
-    % For some reason, this can lead to about a 400-micron up-and-down. So
-    % give us space!
-    mompos = move('mom');
-    minmommove = max(min(mompos(3), 400) - 1, 0)
-    disp('Moving MOM to:');
-    mompos - [0 0 minmommove]
-
-    %move('mom', mompos - [ 0 0 minmommove]);
-    STL.motors.hex.C887.VLS(2);
+    STL.motors.hex.C887.VLS(10);
     STL.motors.hex.C887.MOV('U V W', [0 0 0]);
     
     if exist('handles', 'var')
@@ -29,8 +42,9 @@ function hexapod_reset_to_zero_rotation(handles)
     else
         hexapod_wait();
     end
-    disp('Moving MOM to:');
-    mompos
+    %% Return MOM to the previous height
+    %disp('Moving MOM to:');
+    %mompos
 
     %move('mom', mompos);
     if exist('handles', 'var')
