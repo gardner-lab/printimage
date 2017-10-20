ee2a1bea9678ce858c499e88cf188034977afe3a
Ben Pearre <bwpearre@gmail.com>
2017-10-19 21:35:05 -0400

    BUGFIX in falloff: apply correct orientation of baseline image. Also a big re-org: do transformations at time of file load, so everything else sees them!

--- falloff.m
+++ falloff.m
@@ -14,47 +14,48 @@ if true
     imgs{c}.file = 'vignetting_empty_00300_00001.tif';
     imgs{c}.desc = 'Ref';
     imgs{c}.method = 'interpolant';
+    imgs{c}.size = 500;
     
     c = c + 1;
     imgs{c}.file = 'vignetting_sin_00300_00001.tif';
     imgs{c}.desc = 'sin';
     imgs{c}.size = 500;
-    imgs{c}.pos_adj = 0;
+    imgs{c}.pos_adj = 0; % microns, after reversing
     
     c = c + 1;
     imgs{c}.file = 'vignetting_f_sin_00300_00001.tif';
     imgs{c}.desc = 'sin 180^\circ';
     imgs{c}.size = 500;
-    imgs{c}.reverse = true;
+    imgs{c}.rotated = true;
     imgs{c}.pos_adj = -10; % microns, after reversing
     
     c = c + 1;
     imgs{c}.file = 'vignetting_adhoc_00300_00001.tif';
     imgs{c}.desc = 'adhoc';
     imgs{c}.size = 500;
-    imgs{c}.pos_adj = 0;
+    imgs{c}.pos_adj = 0; % microns, after reversing
     
     c = c + 1;
     imgs{c}.file = 'vignetting_f_adhoc_00300_00001.tif';
     imgs{c}.desc = 'adhoc 180^\circ';
     imgs{c}.size = 500;
-    imgs{c}.pos_adj = -10;
-    imgs{c}.reverse = true;
+    imgs{c}.pos_adj = 0; % microns, after reversing
+    imgs{c}.rotated = true;
     
     c = c + 1;
     imgs{c}.file = 'vignetting_interp_00300_00001.tif';
     imgs{c}.desc = 'interp';
     imgs{c}.size = 500;
-    imgs{c}.pos_adj = 0;
+    imgs{c}.pos_adj = 0; % microns, after reversing
     
     c = c + 1;
     imgs{c}.file = 'vignetting_f_interp_00300_00001.tif';
     imgs{c}.desc = 'interp 180^\circ';
     imgs{c}.size = 500;
-    imgs{c}.reverse = true;
+    imgs{c}.rotated = true;
     imgs{c}.pos_adj = 0; % microns, after reversing
     
-    if false
+    if true
         c = c + 1;
         imgs{c}.file = 'vignetting_cos4_00300_00001.tif';
         imgs{c}.desc = 'cos^4';
@@ -65,9 +66,11 @@ if true
         imgs{c}.file = 'vignetting_f_cos4_00300_00001.tif';
         imgs{c}.desc = 'cos^4 180^\circ';
         imgs{c}.size = 500;
-        imgs{c}.reverse = true;
+        imgs{c}.rotated = true;
         imgs{c}.pos_adj = 0; % microns, after reversing
-        
+    end
+    
+    if false
         c = c + 1;
         imgs{c}.file = 'vignetting_cos4free_00300_00001.tif';
         imgs{c}.desc = 'cos^4 free';
@@ -78,7 +81,7 @@ if true
         imgs{c}.file = 'vignetting_f_cos4free_00300_00001.tif';
         imgs{c}.desc = 'cos^4 free 180^\circ';
         imgs{c}.size = 500;
-        imgs{c}.reverse = true;
+        imgs{c}.rotated = true;
         imgs{c}.pos_adj = 0; % microns, after reversing
         
         c = c + 1;
@@ -91,7 +94,7 @@ if true
         imgs{c}.file = 'vignetting_f_cos4gauss_00300_00001.tif';
         imgs{c}.desc = 'cos^4+gauss 180^\circ';
         imgs{c}.size = 500;
-        imgs{c}.reverse = true;
+        imgs{c}.rotated = true;
         imgs{c}.pos_adj = 0; % microns, after reversing
     end
 elseif false
@@ -109,7 +112,7 @@ elseif false
     imgs{c}.file = 'vignetting_f_sin_00200_00001.tif';
     imgs{c}.desc = 'sin 180^\circ';
     imgs{c}.size = 450;
-    imgs{c}.reverse = true;
+    imgs{c}.rotated = true;
     imgs{c}.pos_adj = -10; % microns, after reversing
     
     c = c + 1;
@@ -123,7 +126,7 @@ elseif false
     imgs{c}.desc = 'adhoc 180^\circ';
     imgs{c}.size = 450;
     imgs{c}.pos_adj = -10;
-    imgs{c}.reverse = true;
+    imgs{c}.rotated = true;
     
     c = c + 1;
     imgs{c}.file = 'vignetting_interp_00200_00001.tif';
@@ -135,7 +138,7 @@ elseif false
     imgs{c}.file = 'vignetting_f_interp_00200_00001.tif';
     imgs{c}.desc = 'interp 180^\circ';
     imgs{c}.size = 450;
-    imgs{c}.reverse = true;
+    imgs{c}.rotated = true;
     imgs{c}.pos_adj = 0; % microns, after reversing
 end
 
@@ -145,10 +148,17 @@ calibration_imgs = [1];
 FOV = 666; % microns
 
 for i = 1:length(imgs)
-    imgs{i}.data = imread(imgs{i}.file);
-    if isfield(imgs{i}, 'reverse') & imgs{i}.reverse
+    imgs{i}.data = double(imread(imgs{i}.file));
+    
+    if isfield(imgs{i}, 'rotated') & imgs{i}.rotated
+        % Unrotate
         imgs{i}.data = imgs{i}.data(end:-1:1, end:-1:1);
+        % Normalise by the rotated baseline
+        imgs{i}.data_normed = imgs{i}.data ./ imgs{1}.data(end:-1:1, end:-1:1);
+    else
+        imgs{i}.data_normed = imgs{i}.data ./ imgs{1}.data;
     end
+
     if any(i == calibration_imgs)
         imgs{i}.vignetting_fit = fit_vignetting_falloff(imgs{i}.file, imgs{i}.method, 666);
     end
@@ -171,7 +181,8 @@ for i = 1:length(imgs)
     else
         npixels = 0;
     end
-    imgs{i}.data = double(imgs{i}.data(1+CROPY:end-CROPY, 1+CROPX-npixels:end-CROPX-npixels));
+    imgs{i}.data = imgs{i}.data(1+CROPY:end-CROPY, 1+CROPX-npixels:end-CROPX-npixels);
+    imgs{i}.data_normed = imgs{i}.data_normed(1+CROPY:end-CROPY, 1+CROPX-npixels:end-CROPX-npixels);
 end
 
 pixelposX = pixelpos(1+CROPX:end-CROPX);
@@ -186,6 +197,7 @@ for i = 2:2:n_real_imgs
     imgs{c}.desc = sprintf('%s comb', imgs{i}.desc);
     imgs{c}.size = imgs{i}.size;
     imgs{c}.data = (imgs{i}.data + imgs{i+1}.data)/2;
+    imgs{c}.data_normed = (imgs{i}.data_normed + imgs{i+1}.data_normed)/2;
 end
 
 
@@ -218,10 +230,12 @@ for i = 1:length(imgs)
         title(imgs{i}.desc);
         
         subplot(sp1, sp2, (i-1)*sp2+3);
-        imagesc(imgs{i}.vignetting_falloff);
+        %imagesc(imgs{i}.vignetting_falloff);
+        imagesc(imgs{i}.data(end:-1:1,end:-1:1));
         axis off xy;
         % colorbar;
-        title(strcat(imgs{i}.desc, ' model'));
+        %title(strcat(imgs{i}.desc, ' model'));
+        
     else
         % Raw data / calibration images
         subplot(sp1, sp2, (i-1)*sp2+1);
@@ -231,45 +245,34 @@ for i = 1:length(imgs)
         axis xy;
         axis off;
 
-        imgs2{i}.data = imgs{i}.data ./ imgs{1}.data;
-        imgs2{i}.data_f = imgs{i}.data ./ imgs{1}.vignetting_falloff;
-        ulim = max(max(imgs2{i}.data));
-        imgs2{i}.data_f(find(imgs2{i}.data_f > ulim)) = ulim;
-        
         % Save baseline luminance for zero-power region
-        imgs2{i}.zeropower_i = find((pixelposX < -imgs{i}.size/2 - 10 & pixelposX > -imgs{i}.size/2 - 20) ...
+        imgs{i}.zeropower_i = find((pixelposX < -imgs{i}.size/2 - 10 & pixelposX > -imgs{i}.size/2 - 20) ...
             | (pixelposX > imgs{i}.size/2 + 10 & pixelposX < imgs{i}.size/2 + 20));
 
         % Plotting
         subplot(sp1, sp2, (i-1)*sp2+2);
-        imagesc(imgs2{i}.data);
+        imagesc(imgs{i}.data_normed);
         axis off xy;
         % colorbar;
         title(strcat(imgs{i}.desc, ' /  ', imgs{1}.desc));
     end
 end
 
-
-
 colours = distinguishable_colors(length(imgs));
 h = [];
 l = {};
 ylimits = [Inf -Inf];
-for i = 1:length(imgs)
-    if isempty(imgs2{i})
-        continue;
-    end
-    
+for i = 2:length(imgs)    
     samplesY = round(0.95*(imgs{i}.size/2)/MicronsPerPixel);
     
-    middleX = round(size(imgs2{i}.data, 2) ./ 2);
-    middleY = round(size(imgs2{i}.data, 1) ./ 2);
-    AVG{i} = mean(imgs2{i}.data(middleY-samplesY:middleY+samplesY, :), 1);
+    middleX = round(size(imgs{i}.data_normed, 2) ./ 2);
+    middleY = round(size(imgs{i}.data_normed, 1) ./ 2);
+    AVG{i} = mean(imgs{i}.data_normed(middleY-samplesY:middleY+samplesY, :), 1);
     % Bring object down to baseline of 0
     AVG{i} = AVG{i} - mean(AVG{i}(middleX-2:middleX+2));
     % Bring unprinted background up to 1
-    AVG{i} = AVG{i} / mean(AVG{i}(imgs2{i}.zeropower_i));
-    STD{i} = std(imgs2{i}.data(middleY-samplesY:middleY+samplesY, :), [], 1);
+    AVG{i} = AVG{i} / mean(AVG{i}(imgs{i}.zeropower_i));
+    STD{i} = std(imgs{i}.data_normed(middleY-samplesY:middleY+samplesY, :), [], 1);
     
     subplot(sp1, sp2, (i-1)*sp2+3);
     H = shadedErrorBar(pixelposX, AVG{i}, 1.96*STD{i}/sqrt(length(samplesY)));
@@ -280,18 +283,16 @@ for i = 1:length(imgs)
     h(end+1) = H.mainLine;
     l{end+1} = imgs{i}.desc;
 end
-%hold off;
-%grid on;
-%legend(h, l);
-%title('Brightness / Ref');
-%axis tight;
 
+% All the plots should have the same scale.
 ylimits(2) = 1.2;
 for i = 2:length(imgs)
     subplot(sp1, sp2, (i-1)*sp2+3);
     set(gca, 'YLim', ylimits);
 end
 
+
+% A separate plot of the combined fits.
 figure(14);
 cla;
 hold on;
@@ -315,34 +316,3 @@ ylabel('Normalised brightness');
 
 
 
-if false
-    subplot(sp1, sp2, 7);
-    cla;
-    hold on;
-    h = [];
-    l = {};
-    for i = 1:length(imgs)
-        if isempty(imgs2{i})
-            continue;
-        end
-        
-        middleX = round(size(imgs2{i}.data, 2) ./ 2);
-        middleY = round(size(imgs2{i}.data, 1) ./ 2);
-        AVG{i} = mean(imgs2{i}.data_f(middleY-100:middleY+100, :), 1);
-        AVG{i} = AVG{i} - mean(AVG{i}(middleX-2:middleX+2));
-        AVG{i} = AVG{i} / mean(AVG{i}(imgs2{i}.zeropower_i));
-        STD{i} = std(imgs2{i}.data_f(middleY-100:middleY+100, :), [], 1);
-        
-        H = shadedErrorBar(pixelposX, AVG{i}, 1.96*STD{i}/sqrt(21), 'lineprops', {'Color', colours(i+1,:)}, 'transparent', 1);
-        h(end+1) = H.mainLine;
-        l{end+1} = imgs{i}.desc;
-    end
-    hold off;
-    legend(h, l);
-    grid on;
-    title('Brightness / Model');
-    axis tight;
-    ylim(yl);
-    
-    colormap jet;
-end
