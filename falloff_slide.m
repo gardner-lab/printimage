d4203318f13741e9081d87c163fc79ce799703fe
Ben <bwpearre@gmail.com>
2017-11-01 16:17:50 -0400

    Correct name of output file.

--- falloff_slide.m
+++ falloff_slide.m
@@ -1,5 +1,5 @@
-function falloff
-
+%function falloff
+clear
 %addpath('~/Downloads');
 
 %imgs{1}.file = 'picture_00006_00001.tif';
@@ -19,20 +19,30 @@ function falloff
 % cos(ax^2+b^2...)
 sz = 500;
 
-%methods = {'sin', 'adhoc', 'cos4', 'interp'};
-methods = {'sin_3', 'adhoc_3', 'interp_3', 'cos^4_3', 'cos^3_3', 'cos3b', 'cos3c'};
-methods = {'cos^3_3', 'cos3b', 'cos3c'};
-methods = {'sin_5', 'adhoc_5', 'interp_5', 'cos^4_5', 'cos^3_5'};
+
+methods = {'sin_6', 'interp_6', 'cos^4_6', 'cos^3_6'};
 FOV = 666; % microns
 how_much_to_include = .95;
 speed = 100; % um/s of the sliding stage
 frame_rate = 15.21; % Hz
 frame_spacing = speed / frame_rate;
-methods2 = strtok(methods, '_');
+
+letter = 'c';
+for i = 1:length(methods)
+    letter = char(letter+1);
+    methods2{i} = sprintf('(%c) %s', letter, strtok(methods{i}, '_'));
+end
 image_crop_x = 20;
 image_crop_y = 40;
 
-figure(16);
+figure(19);
+if exist('p', 'var')
+    delete(p);
+end
+p = panel();
+p.pack(3, 1);
+p(1,1).marginbottom = 100;
+make_sine_plot_3(p(1,1));
 
 tiffCal = double(imread('vignetting_cal_00001_00001.tif'));
 
@@ -42,7 +52,7 @@ for f = 1:length(methods)
     tiffS{f} = tiffS{f}(1+image_crop_y:end-image_crop_y, 1+image_crop_x:end-image_crop_x);
     
     tiffX{f} = [];
-    i = 0;    
+    i = 0;
     try
         while true
             i = i + 1;
@@ -61,7 +71,7 @@ for f = 1:length(methods)
     % Normalise brightness
     baselineX = mean(mean(tiffX{f}(1:30,indices,middle), 2), 1);
     tiffX{f} = tiffX{f}/baselineX;
-
+    
     bright_x(f,:) = mean(tiffX{f}(:, indices, middle), 2);
     bright_x_std(f,:) = std(tiffX{f}(:,indices, middle), [], 2);
     
@@ -79,7 +89,7 @@ for f = 1:length(methods)
     % Normalise brightness
     baselineY = mean(mean(tiffY{f}(1:30,middle,indices), 3), 1);
     tiffY{f} = tiffY{f}/baselineY;
-
+    
     
     bright_y(f,:) = mean(tiffY{f}(:, middle, indices), 3);
     bright_y_std(f,:) = std(tiffY{f}(:, middle, indices), [], 3);
@@ -88,18 +98,25 @@ end
 colours = distinguishable_colors(length(methods));
 
 
+p(2,1).pack(1, length(methods));
+
 for f = 1:length(methods)
-    subplot(2, length(methods), f)
-    imagesc(tiffS{f});
+    p(2,1, 1,f).select();
+    cla;
+    foo = min((tiffS{f} - min(min(tiffS{f}))), 2);
+    
+    imagesc(foo);
     title(methods2{f});
     axis equal off;
+    colormap gray;
 end
 
-subplot(2,2,3);
+p(3,1).pack(1, 2);
+p(3,1,1,1).select();
 cla;
 h = [];
 hold on;
-scanposX = (1:size(bright_x, 2)) * frame_spacing;
+scanposX = (1:size(bright_x, 2)) * frame_spacing - 500;
 
 for f = 1:length(methods)
     H = shadedErrorBar(scanposX, ...
@@ -112,17 +129,18 @@ end
 hold off;
 axis tight;
 ylimits = get(gca, 'YLim');
-set(gca, 'XLim', [100 900]);
+set(gca, 'XLim', [-400 400]);
 legend(h, strtok(methods, '_'), 'Location', 'North');
-title('X brightness');
+letter = letter + 1;
+title(sprintf('(%c) X brightness', letter));
 xlabel('\mu{}m');
 ylabel('normalised brightness');
 
-subplot(2,2,4);
+p(3,1,1,2).select();
 cla;
 h = [];
 hold on;
-scanposY = (1:size(bright_y, 2)) * frame_spacing;
+scanposY = (1:size(bright_y, 2)) * frame_spacing - 500;
 for f = 1:length(methods)
     H = shadedErrorBar(scanposY, ...
         bright_y(f,:), ...
@@ -133,15 +151,20 @@ for f = 1:length(methods)
 end
 hold off;
 axis tight;
-set(gca, 'XLim', [100 900]);
+set(gca, 'XLim', [-400 400]);
 legend(h, strtok(methods, '_'), 'Location', 'North');
-title('Y brightness');
+letter = letter + 1;
+title(sprintf('(%c) Y brightness', letter));
 xlabel('\mu{}m');
 ylabel('normalised brightness');
 
 ylimits2 = get(gca, 'YLim');
 ylimits = [min(ylimits(1), ylimits2(1)) max(ylimits(2), ylimits2(2))];
 set(gca, 'YLim', ylimits);
-subplot(2,2,3);
+p(3,1,1,1).select();
 set(gca, 'YLim', ylimits);
 
+%Figure sizing
+%pos = get(gcf, 'Position');
+%set(gcf, 'Units', 'inches', 'Position', [pos(1) pos(2) 10 8])
+p.export('beamgraph.eps', '-w250', '-a1.2');
---
+++ make_sine_plot_3.m
@@ -0,0 +1,114 @@
+% Make the sine plots. Takes one argument: the (sub?)panel into which to plot.
+function make_sine_plot_3(p)
+    
+    %Number of time samples in a full phase:
+    nsteps = 3333333 / 7980;
+    
+    %Time
+    t = linspace(-pi, pi, nsteps);
+    
+    %Beam Velocity
+    if isempty(p.children)
+        p.pack(1,3);
+    end
+    
+    p(1,1).select();
+
+    x = cos(t);
+    ti = find(t >= -pi/2 & t <= pi/2);
+    plot(t(ti), x(ti), 'LineWidth', 2); hold on;
+    xlabel('Time (phase)');
+    ylabel('Velocity');
+    
+    %Format
+    xlim([-1.8, 1.8]);
+    ylim([0, 1]);
+    set(gca, 'box', 'off', 'TickDir', 'out');
+    set(gca, 'XTick', [-pi/2, 0, pi/2], 'XTickLabel', {'-\pi/2', 0, '\pi/2'})
+    set(gca, 'YTick', [0, 0.5, 1], 'YTickLabel', [0, 0.5, 1])
+    title('(a) Beam velocity');
+    
+    %Beam position
+    p(1,2).select();
+    % labels = {'Resonant scanner beam position'};
+    x = sin(t);
+    ti = find(t >= -pi/2 & t <= pi/2);
+    plot(t(ti), x(ti), 'LineWidth', 2);
+    hold on;
+    tt = asin(0.9);
+    
+    % Imaging fraction of beam. Show voxel positions for slower control system for clarity...
+    nsteps_show = 1000000 / 7980;
+    t_show = linspace(-pi, pi, nsteps_show);
+    x_show = sin(t_show);
+    D = 0.9;
+    tt = asin(0.9);
+    ti = find(t > -tt & t < tt);
+    ti_show = find(t_show > -tt & t_show < tt);
+    %plot(t_show(ti_show),x_show(ti_show), 'k.', 'MarkerSize', 10);
+    
+    sz = 0.1;
+    lines_x = [t_show(ti_show)-sz; t_show(ti_show)+sz];
+    lines_y = [x_show(ti_show); x_show(ti_show)];
+    line(lines_x, lines_y, 'Color', [0 0 0], 'LineWidth', 0.1);
+    
+    %hold off;
+    ylabel('X Position');
+    xlabel('Time (phase)');
+    %xlim([-pi,pi]);
+    legend({'Beam position', 'Voxels'}, 'Location', 'NorthWest', 'box', 'off');
+    %title('(A) Beam position through time');
+    title('(b) Voxel positions');
+    %Format
+    xlim([-1.8, 1.8]);
+    ylim([-1, 1]);
+    set(gca, 'box', 'off', 'TickDir', 'out');
+    set(gca, 'XTick', [-pi/2, 0, pi/2], 'XTickLabel', {'-\pi/2', 0, '\pi/2'})
+    set(gca, 'YTick', [-1, 0, 1], 'YTickLabel', {'-\xi', 0, '\xi'})
+
+    p(1,3).select();
+    % Power compensation
+    p = cos(t);
+    pstar = 0.5*(1+cos(t));
+    
+    %% cos^4 vignetting compensation. Show it for zoom=2.2x, since that's what
+    %% pstar is calibrated to. That means that the usable part of the X axis has
+    %% size FOV/zoom, the lens working distance
+    lens_working_distance = 380; % um
+    fov = 666; % um for whole FOV
+    zoomlevel = 1.3;
+    
+    
+    convert_phase_dist_to_microns = fov/(D*zoomlevel*2);
+    positions_um = sin(t) * convert_phase_dist_to_microns;
+    disp(sprintf('Working FOV is %g um', D*(max(positions_um)-min(positions_um))));
+    %= lens_working_distance * convert_microns_to_phase_dist;
+    % Divide p by predicted vignetting compensation
+    cos3 = cos(atan(positions_um./lens_working_distance)).^2;
+    cos4 = cos(atan(positions_um./lens_working_distance)).^4;
+    
+    plot(t(ti), p(ti), 'r', ...
+        t(ti), cos3(ti), 'g', ...
+        t(ti), p(ti)./cos4(ti), 'k', ...
+        t(ti), p(ti)./cos3(ti), 'b');
+    %    t(ti), pstar(ti), 'c', ... % Ad-hoc
+    ylabel('Power');
+    xlabel('Time (phase)');
+    % xlim([-pi,pi]);
+    % ylim([0 1.1]);
+    legend({'Beam speed (cos(t))', 'Vignetting falloff (cos^3(x))', 'Compensation (cos(t)/cos^4(x))', 'Compensation (cos(t)/cos^3(x))' }, ...
+        'Location', 'South', 'box', 'off');
+    %title('(B) Relative voxel size');
+    title(sprintf('(c) Power compensation at FOV = 512 \\mu{}m'));
+    %Format
+    xlim([-1.8, 1.8]);
+    yl = get(gca, 'YLim');
+    ylim([0, 1.3]);
+    set(gca, 'box', 'off', 'TickDir', 'out');
+    set(gca, 'XTick', [-pi/2, 0,pi/2], 'XTickLabel', {'-\pi/2', 0, '\pi/2'})
+    set(gca, 'YTick', [0, 0.5, 1, 1.5, 2], 'YTickLabel', [0, 0.5, 1, 1.5, 2])
+    %set(gca, 'YLim', [0.35 2]);
+    
+    %Figure sizing
+    %p = get(gcf, 'Position');
+    %set(gcf, 'Units', 'inches', 'Position', [p(1) p(2) 5 8])
