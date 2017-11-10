2b249dc66b36b5e7c4c5233164add0aeecc5b135
Ben Pearre <bwpearre@gmail.com>
2017-11-10 18:43:56 -0500

    Don't draw the graph in show_power_adjustment.

--- show_power_adjustment.m
+++ show_power_adjustment.m
@@ -9,18 +9,4 @@ function show_power_adjustment
     
     ao_volts_out = printimage_modify_beam(ao_volts_raw);
     x = reshape(ao_volts_out.B, STL.print.metavoxel_resolution{1,1,1});
-    
-    figure(34);
-    subplot(1,2,2);
-    %mesh(1:152, 1:512, squeeze(x(:,:,end))');
-    imagesc(STL.print.voxelpos_wrt_fov{1,1,1}.x, ...
-        STL.print.voxelpos_wrt_fov{1,1,1}.y, ...
-        squeeze(x(:,:,end))');
-    axis square;
-    colorbar;
-    colormap(jet);
-    title('Power compensation');
-    xlabel('X (\mu{}m)');
-    ylabel('Y (\mu{}m)');
-    %zlabel('Laser power');
 end
