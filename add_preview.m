0fa0db31ca4fb83b5f3817e7f224c52df81a3689
Ben <bwpearre@gmail.com>
2018-04-24 14:43:13 -0400

    Debug preview (remotely, so this doesn't really warrant a checkin... but...)

--- add_preview.m
+++ add_preview.m
@@ -4,13 +4,21 @@ function add_preview(handles)
     hSICtl = evalin('base', 'hSICtl');
     
     relevant_images = [];
+    irrelevant_images = [];
     for i = 1:length(hSICtl.hManagedGUIs)
         if (strcmp(hSICtl.hManagedGUIs(i).Name, 'Channel 1') ...
                 | strcmp(hSICtl.hManagedGUIs(i).Name, 'Channel 2') ...
                 | strcmp(hSICtl.hManagedGUIs(i).Name, 'Channel Merge')) ...
                 & strcmp(hSICtl.hManagedGUIs(i).Visible, 'on')
             relevant_images = [relevant_images i];
+        else
+                irrelevant_images = [irrelevant_images i];
         end
+    end
+    relevant_images
+    irrelevant_images
+    if length(relevant_images) == 0
+            warning('Ben''s assumptions about what the relevant fields are in hSICtl.hManagedGUIs seem to be wrong.');
     end
     
     % Need to know the bounds that we'll be printing at. That's
