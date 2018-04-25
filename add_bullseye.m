7dbcdbf8ad9f5d8504bbc43db2474f9a733b5622
Ben <bwpearre@gmail.com>
2018-04-25 17:11:35 -0400

    Better modularisation for draw_on_image stuff.

--- add_bullseye.m
+++ add_bullseye.m
@@ -3,14 +3,7 @@ function add_bullseye()
     hSI = evalin('base', 'hSI');
     hSICtl = evalin('base', 'hSICtl');
     
-    relevant_images = [];
-    for i = 1:length(hSICtl.hManagedGUIs)
-        if strcmp(hSICtl.hManagedGUIs(i).Name, 'Channel 1') ...
-                | strcmp(hSICtl.hManagedGUIs(i).Name, 'Channel 2') ...
-                | strcmp(hSICtl.hManagedGUIs(i).Name, 'Channel Merge')
-            relevant_images = [relevant_images i];
-        end
-    end
+    relevant_images = draw_on_image_get_images();
     
     fov = hSI.hRoiManager.imagingFovUm;
     bounds = [fov(3,1) - fov(1,1)      fov(3,2) - fov(1,2)];
@@ -23,12 +16,7 @@ function add_bullseye()
         scalelen = sprintf('%d {\\mu}m', scale_bar_length);
         scale_bar_ypos = fov(end,end) - 0.1 * bounds(2);
         
-        z = get(hSICtl.hManagedGUIs(i).CurrentAxes, 'ZLim');
-        if str2double(hSI.VERSION_MAJOR) <= 5.3 & ~str2double(hSI.VERSION_MINOR) > 0
-            z = z(2) - 0.0001; % ScanImage < 5.3.1 wanted the z info drawn here.
-        else
-            z = mean(z); % This _MIGHT_ work with versions < 5.3.1 as well. Can't test due to NIDAQ firmware upgrade...
-        end
+        z = draw_on_image_get_z(hSICtl.hManagedGUIs(i).CurrentAxes);
         
         sizes = round(linspace(2, 18, 6).^2);
         
